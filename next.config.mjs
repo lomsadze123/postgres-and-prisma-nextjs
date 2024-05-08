51955e03caf271034cd8ac9b9d55341443999704
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2024-05-08 21:27:03 +0400

    fix production issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  output: {
+    logVerbose: true,
+  },
+};
 
 export default nextConfig;
